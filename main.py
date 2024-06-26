86f3ec6d18a2065f4c4fc4bc5e6296e61ad86249
raptorblue-david <davidfabianfuentes@gmail.com>
2024-06-26 18:15:01 -0500

    Create main.py
    bot para discord facilísimo verdad

---
+++ main.py
@@ -0,0 +1,48 @@
+import discord
+import random
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+@bot.command()
+async def hola(ctx):
+    await ctx.send(f'Hola, soy el {bot.user}!')
+
+@bot.command()
+async def haha(ctx, count_haha = 5):
+    await ctx.send("haha" * count_haha)
+
+@bot.command()
+async def xd(ctx):
+    await ctx.send(f'xd =)')
+
+bot.run("pon tu token")
